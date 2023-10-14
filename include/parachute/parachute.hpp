d94baa434dd0100afe48b04568f57dc4ec7a5815
Brian Cairl <briancairl@gmail.com>
2023-10-14 14:26:17 -0400

    Adds full-feature meta include

---
+++ include/parachute/parachute.hpp
@@ -0,0 +1,14 @@
+/**
+ * @copyright 2023-present Brian Cairl
+ *
+ * @file parachute.hpp
+ */
+#pragma once
+
+// C++ Standard Library
+#include <future>
+
+// Parachute
+#include <parachute/pool.hpp>
+#include <parachute/post.hpp>
+#include <parachute/non_blocking_future.hpp>
