74903909301ff35073791f4ed7f6e4a9d89eee43
Brian Cairl <briancairl@gmail.com>
2023-10-14 21:27:31 -0400

    Adds algorigtm meta include header

---
+++ include/parachute/algorithm.hpp
@@ -0,0 +1,10 @@
+/**
+ * @copyright 2023-present Brian Cairl
+ *
+ * @file pool.hpp
+ */
+#pragma once
+
+// Parachute
+#include <parachute/algorithm/for_each.hpp>
+#include <parachute/algorithm/transform.hpp>
