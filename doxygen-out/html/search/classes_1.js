e42a63da4a5f8c7dc3165ad7b3b703d69f302fd2
briancairl <briancairl@gmail.com>
2023-10-15 21:48:12 +0000

    Deploying to gh-pages from @ briancairl/parachute@3630d1fcf1b8851dc1cd6fb3114e2e65ebeb4717 🚀

--- doxygen-out/html/search/classes_1.js
+++ doxygen-out/html/search/classes_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['non_5fblocking_5ffuture_81',['non_blocking_future',['../classpara_1_1non__blocking__future.html',1,'para']]],
   ['non_5fblocking_5ffuture_5ferror_82',['non_blocking_future_error',['../structpara_1_1non__blocking__future__error.html',1,'para']]],
   ['non_5fblocking_5fpromise_83',['non_blocking_promise',['../classpara_1_1non__blocking__promise.html',1,'para']]],
-  ['non_5fblocking_5fpromise_3c_20void_20_3e_84',['non_blocking_promise&lt; void &gt;',['../structpara_1_1non__blocking__promise_3_01void_01_4.html',1,'para']]],
+  ['non_5fblocking_5fpromise_3c_20void_20_3e_84',['non_blocking_promise&lt; void &gt;',['../classpara_1_1non__blocking__promise_3_01void_01_4.html',1,'para']]],
   ['non_5fblocking_5fpromise_5fcommon_85',['non_blocking_promise_common',['../classpara_1_1detail_1_1non__blocking__promise__common.html',1,'para::detail']]],
   ['non_5fblocking_5fpromise_5fcommon_3c_20void_20_3e_86',['non_blocking_promise_common&lt; void &gt;',['../classpara_1_1detail_1_1non__blocking__promise__common.html',1,'para::detail']]],
   ['non_5fblocking_5fshared_5fstate_87',['non_blocking_shared_state',['../classpara_1_1detail_1_1non__blocking__shared__state.html',1,'para::detail']]],
